b5a819c2ed0c169f4a3c78082cbbca533fba4c33
Wen Chen <Wen.Chen@amd.com>
2024-10-08 19:05:02 +0000

    Fixed two bugs: * The amax needs to be performed on the original value instead of the scaled value. * Triton conversion to fp8 does not have clipping and therefore we will need to do that in the kernel before conversion.

--- transformer_engine/pytorch/cast_transpose_triton.py
+++ transformer_engine/pytorch/cast_transpose_triton.py
@@ -13,6 +13,12 @@ def reinterpret_as_fp8_tensor(a: torch.Tensor, dtype: tex.DType):
     if dtype == tex.DType.kFloat8E5M2:
         return a.view(dtype=torch.float8_e5m2fnuz)
 
+def get_fp8_max(dtype):
+    if dtype == tex.DType.kFloat8E4M3:
+        return 240.0
+    if dtype == tex.DType.kFloat8E5M2:
+        return 57344.0
+
 '''
 @triton.autotune(
         configs=[
@@ -23,9 +29,8 @@ def reinterpret_as_fp8_tensor(a: torch.Tensor, dtype: tex.DType):
         key=['M', 'N']
 )
 '''
-#@triton.Config({'BLOCK_M': 128, 'BLOCK_N': 128, 'GROUP_M': 8}, num_warps=8)
 @triton.jit
-def _transpose_triton(A, C, T, stride_am, stride_an, stride_bn, stride_bm, M, N, scale_ptr, amax_ptr, BLOCK_M: tl.constexpr, BLOCK_N: tl.constexpr, GROUP_M: tl.constexpr):
+def _transpose_triton(A, C, T, stride_am, stride_an, stride_bn, stride_bm, M, N, scale_ptr, amax_ptr, max_fp8: tl.constexpr, BLOCK_M: tl.constexpr, BLOCK_N: tl.constexpr, GROUP_M: tl.constexpr):
     pid = tl.program_id(0)
     scale = tl.load(scale_ptr)
 
@@ -45,11 +50,12 @@ def _transpose_triton(A, C, T, stride_am, stride_an, stride_bn, stride_bm, M, N,
     a = tl.load(A, mask=mask)
 
     scaled_a = a * scale
+    scaled_a = tl.clamp(scaled_a, -max_fp8, max_fp8)
     fp8_a = scaled_a.to(C.type.element_ty)
     C = C + (rm[:, None] * stride_am + rn[None, :] * stride_an)
     tl.store(C, fp8_a, mask=mask)
     
-    amax = tl.max(tl.abs(scaled_a))
+    amax = tl.max(tl.abs(a.to(tl.float32)))
     tl.atomic_max(amax_ptr, amax, sem='relaxed')
     # rematerialize to save registers
     rm = pid_m * BLOCK_M + tl.arange(0, BLOCK_M)
@@ -67,11 +73,9 @@ def te_cast_transpose_triton(input, input_scale, cast_out, trans_out, amax_out, 
         trans_out = reinterpret_as_fp8_tensor(trans_out, otype)
     
     assert trans_out.size(0) == N and trans_out.size(1) == M
-    assert input.stride(0) == 1 or input.stride(1) == 1
-    assert trans_out.stride(0) == 1 or trans_out.stride(1) == 1
     
     grid = lambda META: (triton.cdiv(M, META['BLOCK_M']) * triton.cdiv(N, META['BLOCK_N']),)
-    _transpose_triton[grid](input, cast_out, trans_out, input.stride(0), input.stride(1), trans_out.stride(0), trans_out.stride(1), M, N, input_scale, amax_out, BLOCK_M=128, BLOCK_N=128, GROUP_M=8, num_warps=8)
+    _transpose_triton[grid](input, cast_out, trans_out, input.stride(0), input.stride(1), trans_out.stride(0), trans_out.stride(1), M, N, input_scale, amax_out, get_fp8_max(otype), BLOCK_M=128, BLOCK_N=128, GROUP_M=8, num_warps=8)
     cast_out = cast_out.view(dtype=torch.uint8)
     trans_out = trans_out.view(dtype=torch.uint8)
     return cast_out, trans_out, amax_out
