b04899828f351a4ebab68a9fad4e4a72bd101268
Ye Wang <yewang12@amd.com>
2025-01-22 16:36:41 -0600

    Add gfx942 into te jax extension build

--- build_tools/jax.py
+++ build_tools/jax.py
@@ -77,6 +77,7 @@ def setup_jax_extension(
         rocm_home, _ = rocm_path()
         macros=[("USE_ROCM",None)]
         cxx_flags.extend(["-D__HIP_PLATFORM_AMD__", "-I{}/include".format(str(rocm_home))])
+        nvcc_flags.extend(["--offload-arch={}".format(os.getenv("NVTE_ROCM_ARCH", "gfx942"))])
     else:
         macros=[]
 
