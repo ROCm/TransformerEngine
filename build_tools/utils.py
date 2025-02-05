52c817cd304e2566515f1c0a1b1daa1cbbbeaec4
Veera Rajasekhar <125210283+VeeraRajasekhar@users.noreply.github.com>
2025-02-05 00:34:51 -0600

    Fixed the hipify_torch error when run with 'setup.py install' (#120)
    * Fixed the hipify_torch error when run with 'setup.py install'
    
    * Made review changes to import hipify_torch instead of sys.path

--- build_tools/utils.py
+++ build_tools/utils.py
@@ -417,11 +417,9 @@ def uninstall_te_wheel_packages():
     )
 
 def hipify(base_dir, src_dir, sources, include_dirs):
-    hipify_path = base_dir / "3rdparty" / "hipify_torch"
     cwd = os.getcwd()
-    os.chdir(hipify_path)
-    from hipify_torch.hipify_python import hipify as do_hipify
-    os.chdir(cwd)
+    hipify_module = importlib.import_module("3rdparty.hipify_torch.hipify_torch.hipify_python")
+    do_hipify = hipify_module.hipify
 
     hipify_result = do_hipify(
         project_directory=src_dir,
