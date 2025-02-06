36e6e39538c34cb7fe3701850982ef2544bdb831
Ilya Panfilov <Ilya.Panfilov@amd.com>
2025-02-06 05:12:32 -0500

    Increase test tolerance on MI308 with hipBalsLt

--- tests/pytorch/test_numerics.py
+++ tests/pytorch/test_numerics.py
@@ -1876,6 +1876,7 @@ def test_transformer_layer_hidden_states_format(dtype, bs, model):
             tols = dtype_tols(dtype)
             if dtype in (torch.float16, torch.bfloat16) and is_mi308():
                 # mi308 hipblaslt precision issue
+                tols["rtol"] = tols["rtol"] * 3
                 tols["atol"] = 2e-3
             torch.testing.assert_close(y_bshd, y_sbhd.transpose(0, 1).contiguous(), **tols)
         else:
