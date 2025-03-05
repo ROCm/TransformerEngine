ef95fe1a1194f9ee0b736d8617f787cc402b235b
Veera Rajasekhar <veerarajasekharreddy.gopu@amd.com>
2025-03-05 18:54:25 +0000

    Fixed setup.py conflict with DCMAKE_CUDA_ARCHITECTURES

--- setup.py
+++ setup.py
@@ -60,7 +60,6 @@ class TimedBdist(bdist_wheel):
 
 def setup_common_extension() -> CMakeExtension:
     """Setup CMake extension for common library"""
-    cmake_flags = ["-DCMAKE_CUDA_ARCHITECTURES={}".format(cuda_archs())]
     if bool(int(os.getenv("NVTE_UB_WITH_MPI", "0"))):
         assert (
             os.getenv("MPI_HOME") is not None
