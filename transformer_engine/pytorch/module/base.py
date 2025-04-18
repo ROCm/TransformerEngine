6f4212d017b71bfdf9a91c1083d24121d1fefe3b
Sarthak Arora <sarthak.arora@amd.com>
2025-04-18 15:21:56 -0500

    transpose cache on/off switch

--- transformer_engine/pytorch/module/base.py
+++ transformer_engine/pytorch/module/base.py
@@ -991,7 +991,7 @@ class TransformerEngineBaseModule(torch.nn.Module, ABC):
                     fp8_meta=self.fp8_meta,
                     fp8_meta_index=fp8_meta_index,
                     amax=dummy_amax,
-                    with_transpose_cache=torch.is_grad_enabled(),
+                    with_transpose_cache=torch.is_grad_enabled() and os.getenv("ENABLE_TRANSPOSE_CACHE", True),
                 )
 
             # Redo parameter wrap in case we broke it above
