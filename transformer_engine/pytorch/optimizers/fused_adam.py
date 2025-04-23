Fix wrong FP8 scaling in some cases

transformer_engine/pytorch/optimizers/fused_adam.py
@@ -12,9 +12,9 @@ import torch
 import transformer_engine_torch as tex
 from transformer_engine.pytorch.float8_tensor import Float8Tensor
 from transformer_engine.pytorch.fp8 import FP8GlobalStateManager
+from transformer_engine.pytorch.utils import is_fp8_fnuz
 from .multi_tensor_apply import multi_tensor_applier
 from ..float8_tensor import Float8Tensor
-from torch.utils.cpp_extension import IS_HIP_EXTENSION
 
 
 def get_fp8_meta(fp8_tensor):
@@ -202,7 +202,7 @@ class FusedAdam(torch.optim.Optimizer):
             torch.float16: torch.full(
                 [1], torch.finfo(torch.float16).max / 2.0, dtype=torch.float32
             ),
-            torch.uint8: torch.full([1], 240.0 if IS_HIP_EXTENSION else 448.0, dtype=torch.float32),
+            torch.uint8: torch.full([1], 240.0 if is_fp8_fnuz() else 448.0, dtype=torch.float32),
         }
         self._scales = {}
         self.use_decoupled_grad = use_decoupled_grad

transformer_engine/pytorch/triton/permutation.py
@@ -12,11 +12,12 @@ import torch
 import triton
 import triton.language as tl
 
+from transformer_engine.pytorch.utils import is_fp8_fnuz
 from transformer_engine_torch import DType as TE_DType
 
 from torch.utils.cpp_extension import IS_HIP_EXTENSION
 
-if IS_HIP_EXTENSION:
+if is_fp8_fnuz():
     e5m2_data_type = tl.float8e5b16
     e4m3_data_type = tl.float8e4b8
 else: