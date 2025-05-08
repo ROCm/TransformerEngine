e61473dd8d82f9939caed973ec461d2aa0f76993
mehmet cagri kaymak <mehmet.kaymak@amd.com>
2025-05-08 21:55:52 +0000

    [ROCm] add alternative dw_db kernel and tune kernels for layernorm bwd

--- transformer_engine/pytorch/triton_kernels/norm_common_triton.py
+++ transformer_engine/pytorch/triton_kernels/norm_common_triton.py
@@ -53,3 +53,21 @@ def block_size(x):
 
 def use_blocked(x):
     return x.shape[1] > block_size(x)
+
+
+def block_size_bwd(x, tile_num):
+    max_fused_size = 65536 // (x.element_size() * 2)
+    next_power = triton.next_power_of_2(x.shape[1])
+    block_size = min(max_fused_size, next_power)
+    # For cases with small M and large N, decrease block size to help with register spill
+    if tile_num == x.shape[0]:
+        # for better occupancy
+        if tile_num > 256:
+            block_size = min(block_size, 2048)
+        else:
+            block_size = min(block_size, 4096)
+    return block_size
+
+
+def use_blocked_bwd(x, tile_num):
+    return x.shape[1] > block_size_bwd(x, tile_num)
