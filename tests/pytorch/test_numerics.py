Fixed hipblaslt precision issue with mi300 on rocm6.4 & hipblaslt12 docker

tests/pytorch/test_numerics.py
@@ -1972,7 +1972,7 @@ def test_transformer_layer_hidden_states_format(dtype, bs, model):
     if IS_HIP_EXTENSION:
         if use_hipblaslt():
             tols = dtype_tols(dtype)
-            if dtype in (torch.float16, torch.bfloat16) and is_mi308():
+            if dtype in (torch.float16, torch.bfloat16):
                 # mi308 hipblaslt precision issue
                 tols["atol"] = 2e-3
                 _, use_aotriton, use_ck = rocm_attn_backend()