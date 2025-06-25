475a0eec707934da1b4f3eb2872a0e7d673a6a19
alextmagro <alex.magro@amd.com>
2025-06-25 09:25:45 -0500

    Remove artifacts from ci/pytorch.sh (#213)
    * Remove remaining references to  in ci/pytorch causing tests to be skipped.
    
    * Remove onnx & onnxruntime installation
    
    ---------
    
    Co-authored-by: Alex Magro <alemagro@amd.com>

--- ci/pytorch.sh
+++ ci/pytorch.sh
@@ -12,7 +12,7 @@ TEST_DIR=${TE_PATH}tests/pytorch
 #: ${TEST_WORKERS:=4}
 
 install_prerequisites() {
-    pip install 'numpy>=1.22.4,<2.0' onnx onnxruntime pandas
+    pip install 'numpy>=1.22.4,<2.0' pandas
     rc=$?
     if [ $rc -ne 0 ]; then
         script_error "Failed to install test prerequisites"
@@ -53,14 +53,11 @@ run_test_config(){
     run 1 test_gqa.py
     run 1 test_jit.py
     run_default_fa 1 test_multi_tensor.py
-    # test_numerics now contains fp8+grad and other gemm configs not supported by rocblas gemm path
-    test $_gemm = "hipblaslt" && run 1 test_numerics.py
+    run 1 test_numerics.py
     # TODO: release test_permutation_mask_map_fp8 until upstream fixes the to_float8 error
     run_default_fa 1 test_permutation.py -k "not test_permutation_mask_map_fp8 and not test_permutation_single_case"
-    # test_recipe now contains fp8+grad and other gemm configs not supported by rocblas gemm path
-    test $_gemm = "hipblaslt" && run_default_fa 1 test_recipe.py
-    # test_sanity now contains fp8+grad and other gemm configs not supported by rocblas gemm path
-    test $_gemm = "hipblaslt" && run 1 test_sanity.py
+    run_default_fa 1 test_recipe.py
+    run 1 test_sanity.py
     run_default_fa 1 fused_attn/test_fused_attn.py # Backend selection is controlled by the test
     # TODO: bring back cast transpose kernels after triton kernels for transformer_engine::pytorch::quantize
     #run_default_fa 1 triton_kernels/test_cast_transpose.py
@@ -111,9 +108,9 @@ fi
 #Master script mode: prepare testing prerequisites first
 start_message
 install_prerequisites
-pip list | egrep "flash|ml_dtypes|numpy|onnx|torch|transformer_e|typing_ext"
+pip list | egrep "flash|ml_dtypes|numpy|torch|transformer_e|typing_ext"
 #check_test_jobs_requested && init_test_jobs `python -c "import torch; print(torch.cuda.device_count())"`
-    
+
 for _fus_attn in auto flash ck aotriton unfused; do
     configure_fused_attn_env $_fus_attn || continue
 
