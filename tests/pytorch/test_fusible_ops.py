6d90d77c1301c3f80340f8c0d143709f7671922a
Meekail Zain <zainmeekail@gmail.com>
2025-07-15 16:04:29 -0500

    Updated license/copyright

--- tests/pytorch/test_fusible_ops.py
+++ tests/pytorch/test_fusible_ops.py
@@ -1,5 +1,7 @@
-# Copyright (c) 2022-2025, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# This file was modified for portability to AMDGPU
 # Copyright (c) 2022-2025, Advanced Micro Devices, Inc. All rights reserved.
+# Copyright (c) 2022-2025, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+#
 # See LICENSE for license information.
 
 from __future__ import annotations
