Fixed current scaling pytests, added new tests to ci/pytorch.sh

tests/pytorch/references/ref_per_tensor_cs.py
@@ -6,6 +6,7 @@ import torch
 import transformer_engine_torch as tex
 
 from transformer_engine.pytorch.constants import TE_DType_To_Torch
+from torch.utils.cpp_extension import IS_HIP_EXTENSION
 
 
 # compute amax and scale
@@ -40,8 +41,23 @@ def _ref_compute_amax_scale(x, quant_dtype, eps, pow_2_scales):
         # rather than allowing the full range of FP32 (2 - 2^23) x 2^127
         # addresses cases where adding a mantissa overflows into inf scales.
         # Not necessary currently without additional scale smudging options.
-        unity = torch.tensor([1.0], device=exp.device)
-        torch.ldexp(unity, exp, out=scale)
+
+        if IS_HIP_EXTENSION:
+            # NOTE: On ROCm/HIP the device-side implementation of torch.ldexp starts to lose
+            # precision once the exponent exceeds 15, so the result is no
+            # longer an exact power-of-two.  The current-scaling tests require an
+            # exact 2^N value when pow_2_scales=true, so for HIP we perform the ldexp on the CPU
+            # and copy the perfectly rounded value back to the original device.
+            # TODO: Remove this once the issue is fixed.
+            host = exp.device
+            scale_cpu = scale.to(device='cpu')
+            exp_cpu = exp.to(device='cpu')
+            unity = torch.tensor([1.0], device='cpu')
+            torch.ldexp(unity, exp_cpu, out=scale_cpu)
+            scale = scale_cpu.to(device=host)
+        else:
+            unity = torch.tensor([1.0], device=exp.device)
+            torch.ldexp(unity, exp, out=scale)
         # Case where amax is inf. The frexp, ldexp logic changes 0.0 scales
         # Return 0.0 for 0.0 scale for consistency with non-pow2 scale
         # calculation.