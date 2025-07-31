f8469389120036093cd258f28a061af27a62234e
Sudharshan Govindan <sugovind@amd.com>
2025-07-31 06:13:40 +0000

    Added guard to base.py

--- transformer_engine/pytorch/module/base.py
+++ transformer_engine/pytorch/module/base.py
@@ -1071,9 +1071,12 @@ class TransformerEngineBaseModule(torch.nn.Module, ABC):
             if hasattr(out, "quantize_"):
                 out.quantize_(tensor, noop_flag=skip_update_flag)
             else:
-                use_cast_transpose_triton =  bool( int(os.environ.get('NVTE_USE_CAST_TRANSPOSE_TRITON', '0')) )
-                quantize_func = te_quantize_triton if use_cast_transpose_triton else tex.quantize
-                quantize_func(tensor, quantizer, out, skip_update_flag)
+                if IS_HIP_EXTENSION:
+                    use_cast_transpose_triton =  bool( int(os.environ.get('NVTE_USE_CAST_TRANSPOSE_TRITON', '0')) )
+                    quantize_func = te_quantize_triton if use_cast_transpose_triton else tex.quantize
+                    quantize_func(tensor, quantizer, out, skip_update_flag)
+                else:
+                    tex.quantize(tensor, quantizer, out, skip_update_flag)
 
         return out
 
--- transformer_engine/pytorch/tensor/float8_tensor.py
+++ transformer_engine/pytorch/tensor/float8_tensor.py
@@ -85,11 +85,12 @@ class Float8Quantizer(Quantizer):
             src = src.contiguous()
 
         # Launch cast kernel
-        quantize_func = tex.quantize
         if IS_HIP_EXTENSION:
             use_cast_transpose_triton =  bool( int(os.environ.get('NVTE_USE_CAST_TRANSPOSE_TRITON', '0')) )
             quantize_func = te_quantize_triton if use_cast_transpose_triton else tex.quantize
-        quantize_func(src, self, dst, noop_flag)
+            quantize_func(src, self, dst, noop_flag)
+        else:
+            tex.quantize(src, self, dst, noop_flag)
 
         # Update FP8 dtype
         dst._fp8_dtype = self.dtype
