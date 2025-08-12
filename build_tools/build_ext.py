2e5a52f1d5f1d5c9bed315374e127ddd3bb65b1a
AllenFarcas <alfarcas@amd.com>
2025-08-12 15:37:07 -0500

    [Fix] Ninja generator usage enforced explicitly, with checks added.

--- build_tools/build_ext.py
+++ build_tools/build_ext.py
@@ -79,8 +79,9 @@ class CMakeExtension(setuptools.Extension):
 
         # Check whether parallel build is restricted
         max_jobs = get_max_jobs_for_parallel_build()
-        if found_ninja():
-            configure_command.append("-GNinja")
+        if not found_ninja():
+            raise RuntimeError(f"This project requires the Ninja build system. Install it using 'pip install ninja'.")
+        configure_command.append("-GNinja")
         build_command.append("--parallel")
         if max_jobs > 0:
             build_command.append(str(max_jobs))
