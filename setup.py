368975f4e986b0b033389cb7dac4d319b929ca7c
ipanfilo <145064111+ipanfilo@users.noreply.github.com>
2025-08-19 13:41:58 -0400

    Add missing pytorch extension dependency (#273)
    (cherry picked from commit 578da7f94c0ec0f062fbe6fb50956c5253abf9ac)

--- setup.py
+++ setup.py
@@ -132,7 +132,9 @@ def setup_requirements() -> Tuple[List[str], List[str], List[str]]:
     # Framework-specific requirements
     if not bool(int(os.getenv("NVTE_RELEASE_BUILD", "0"))):
         if "pytorch" in frameworks:
-            if not rocm_build():
+            if rocm_build():
+                install_reqs.extend(["einops"])
+            else:
                 install_reqs.extend(["torch>=2.1"])
                 # Blackwell is not supported as of Triton 3.2.0, need custom internal build
                 # install_reqs.append("triton")
