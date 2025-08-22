def6e2982769533c0fc15b30da73d73b54f687ee
sudhu2k <Sudharshan.Govindan@amd.com>
2025-08-22 11:22:33 -0500

    Ensure weight transpose is valid for FP8 training (#1596) (#276)
    * Update usage of weightmat before saving for backward

--- transformer_engine/pytorch/module/layernorm_linear.py
+++ transformer_engine/pytorch/module/layernorm_linear.py
@@ -340,8 +340,9 @@ class _LayerNormLinear(torch.autograd.Function):
                         ln_out.update_usage(rowwise_usage=False)
 
             # Weight with column-wise usage is needed for dgrad GEMM.
-            if isinstance(weightmat, QuantizedTensor):
-                weightmat.update_usage(columnwise_usage=True)
+            if inp.requires_grad:
+                if isinstance(weightmat, QuantizedTensor):
+                    weightmat.update_usage(columnwise_usage=True)
 
             if cpu_offloading:
                 if fp8 and weightmat is not None:
