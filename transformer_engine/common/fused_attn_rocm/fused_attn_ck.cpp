7dd8e09b374fbbfcc94267463895f9594c99e9d8
sudhu2k <Sudharshan.Govindan@amd.com>
2025-08-25 10:29:21 -0500

    13192 FAv3 tests added to test_fused_atn for jax and pytorch. (#272)
    * Added V3 pytorch tests, Enabled SBHD layout for v3
    
    * Added v3 tests for jax
    
    * Moved env variable to inside the ci scripts
    
    * Removed TODO comment
    
    * Added fused attn FWD V3 tests for jax and pytorch
    
    * Fixed spelling
    
    * Merged forward and backward FAv3 tests into one
    
    * Disabling bwd v3 for varlen code paths
    
    * Reverting unwanted files
    
    * Code cleanup, changed sm_arch_name to sm_arch
    
    * Removed redundant rechecking of nvte_ck_uses_bwd_v3
    
    * Changed nvte_ck_uses_bwd_v3 to not is_gfx95x
    
    ---------
    
    Co-authored-by: Sudharshan Govindan <sugovind@amd.com>

--- transformer_engine/common/fused_attn_rocm/fused_attn_ck.cpp
+++ transformer_engine/common/fused_attn_rocm/fused_attn_ck.cpp
@@ -999,16 +999,20 @@ void fused_attn_ck_bwd_impl(
 
   // bwd v3 is optional by enabling the following envs
   // default values follows the ck example setting
-  // TODO: release SBHD format once CK support it
-  bool nvte_ck_uses_bwd_v3 = getenv<int>("NVTE_CK_USES_BWD_V3", 0) and (nvte_get_qkv_format(layout)!=NVTE_QKV_Format::NVTE_SBHD);
-  if(nvte_log_ck_config){
-    if(getenv<int>("NVTE_CK_USES_BWD_V3", 0) and (nvte_get_qkv_format(layout)==NVTE_QKV_Format::NVTE_SBHD)){
-      std::cout<<"Disable CK BWD v3 since SBHD format not supported"<<std::endl;
-    }
-  }
+  bool nvte_ck_uses_bwd_v3 = getenv<int>("NVTE_CK_USES_BWD_V3", 0);
   bool nvte_ck_is_v3_atomic_fp32 = getenv<int>("NVTE_CK_IS_V3_ATOMIC_FP32", 1);
   int nvte_ck_how_v3_bf16_cvt = getenv<int>("NVTE_CK_HOW_V3_BF16_CVT", 1);
-
+  // TODO: Enable bwd v3 for gfx950 after numerical issues are fixed.
+  if (nvte_ck_uses_bwd_v3 && (pad_between_seqs || is_ragged)){
+    const int gpu_arch = cuda::sm_arch(cuda::current_device());
+    const bool is_gfx95x = gpu_arch == 95;
+    if(nvte_log_ck_config){
+      if(is_gfx95x){
+        std::cout<<"Disabling CK BWD v3 because of numerical issues in gfx950 arch while using varlen backend"<<std::endl;
+      }
+    }
+    nvte_ck_uses_bwd_v3 = !is_gfx95x;
+  }
   if (nvte_log_ck_config) {
     std::cout<<std::endl<<"attn_bwd(ck): ";
     std::cout<<"layout: "<<layout<<", ";
