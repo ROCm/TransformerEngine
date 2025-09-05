479ac65cfa202b2597bb7188f8427bd3ddd98c6c
Ilya Panfilov <Ilya.Panfilov@amd.com>
2025-09-05 10:46:26 -0400

    Fix test running from TE tree root

--- tests/jax/test_sanity_import.py
+++ tests/jax/test_sanity_import.py
@@ -16,15 +16,17 @@ def test_lazy_init():
     os.environ["NVTE_FRAMEWORK"] = "jax"
     ret = subprocess.run(
         [sys.executable, "-c",
+         "import sys; sys.path[:] = [p for p in sys.path if p not in ['', '.']]; "+
          "import os; os.environ['HIP_VISIBLE_DEVICES']=''; import transformer_engine"]
          ).returncode
-    assert ret == 0, "Failed to import TE witout visible devices"
+    assert ret == 0, "Failed to import TE without visible devices"
     dev_count = jax.local_device_count()
     if dev_count >= 2:
-        dev_count_test = subprocess.run([sys.executable, '-c',
-                                        "import transformer_engine, jax, os; " +
-                                        "os.environ['HIP_VISIBLE_DEVICES']='0'; "+
-                                        "exit(jax.local_device_count())"]
-                                        ).returncode
+        dev_count_test = subprocess.run(
+            [sys.executable, '-c',
+             "import sys; sys.path[:] = [p for p in sys.path if p not in ['', '.']]; "+
+             "import transformer_engine, jax, os; os.environ['HIP_VISIBLE_DEVICES']='0'; "+
+             "exit(jax.local_device_count())"]
+             ).returncode
         assert dev_count_test==1, (
             "Changing visible devices after import did not affect reported devices count")
--- tests/pytorch/test_sanity_import.py
+++ tests/pytorch/test_sanity_import.py
@@ -16,15 +16,17 @@ def test_lazy_init():
     os.environ["NVTE_FRAMEWORK"] = "pytorch"
     ret = subprocess.run(
         [sys.executable, "-c",
+         "import sys; sys.path[:] = [p for p in sys.path if p not in ['', '.']]; "+
          "import os; os.environ['HIP_VISIBLE_DEVICES']=''; import transformer_engine"]
          ).returncode
-    assert ret == 0, "Failed to import TE witout visible devices"
+    assert ret == 0, "Failed to import TE without visible devices"
     dev_count = torch.cuda.device_count()
     if dev_count >= 2:
-        dev_count_test = subprocess.run([sys.executable, '-c',
-                                        "import transformer_engine, torch, os; " +
-                                        "os.environ['HIP_VISIBLE_DEVICES']='0'; "+
-                                        "exit(torch.cuda.device_count())"]
-                                        ).returncode
+        dev_count_test = subprocess.run(
+            [sys.executable, '-c',
+             "import sys; sys.path[:] = [p for p in sys.path if p not in ['', '.']]; "+
+             "import transformer_engine, torch, os; os.environ['HIP_VISIBLE_DEVICES']='0'; "+
+             "exit(torch.cuda.device_count())"]
+             ).returncode
         assert dev_count_test==1, (
             "Changing visible devices after import did not affect reported devices count")
