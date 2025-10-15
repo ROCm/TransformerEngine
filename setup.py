404887da38213ab2643d0a7de40786f5623af4a0
Veera Gopu <VeeraRajasekharReddy.Gopu@amd.com>
2025-10-15 11:04:02 +0000

    [TE][AITER] Add prebuilt AITER download and fallback-to-source build flow

--- setup.py
+++ setup.py
@@ -69,6 +69,8 @@ def setup_common_extension() -> CMakeExtension:
         if os.getenv("NVTE_CK_FUSED_ATTN_PATH"):
             ck_path = Path(os.getenv("NVTE_CK_FUSED_ATTN_PATH"))
             cmake_flags.append(f"-DAITER_MHA_PATH={ck_path}")
+        elif bool(int(os.getenv("NVTE_USE_PREBUILT_AITER", "0"))):
+                cmake_flags.append("-DUSE_PREBUILT_AITER=ON")
         if int(os.getenv("NVTE_FUSED_ATTN_AOTRITON", "1"))==0 or int(os.getenv("NVTE_FUSED_ATTN", "1"))==0:
             cmake_flags.append("-DUSE_FUSED_ATTN_AOTRITON=OFF")
         if int(os.getenv("NVTE_FUSED_ATTN_CK", "1"))==0 or int(os.getenv("NVTE_FUSED_ATTN", "1"))==0:
