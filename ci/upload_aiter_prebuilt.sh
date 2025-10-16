84c920088228249a382a10a7d0c8b9ee1f329296
Veera Gopu <VeeraRajasekharReddy.Gopu@amd.com>
2025-10-16 08:37:50 +0000

    Addressed reviews

--- ci/upload_aiter_prebuilt.sh
+++ ci/upload_aiter_prebuilt.sh
@@ -3,14 +3,14 @@
 #
 # See LICENSE for license information
 
-if [ -z "${NVTE_PREBUILT_BASE_URL:-}" ]; then
+if [ -z "${NVTE_PREBUILT_BASE_URL}" ]; then
   echo "[AITER-PREBUILT] [Error] To fetch .so, set NVTE_PREBUILT_BASE_URL." >&2
-  exit 2
+  exit 1
 fi
 
-if [ -z "${NVTE_ARTIFACTORY_USER:-}" ] || [ -z "${NVTE_ARTIFACTORY_PASSWORD:-}" ]; then
+if [ -z "${NVTE_ARTIFACTORY_USER}" ] || [ -z "${NVTE_ARTIFACTORY_PASSWORD}" ]; then
   echo "[AITER-PREBUILT] [Error] To fetch .so, set NVTE_ARTIFACTORY_USER & NVTE_ARTIFACTORY_PASSWORD" >&2
-  exit 2
+  exit 1
 fi
 CURL_AUTH="-u ${NVTE_ARTIFACTORY_USER}:${NVTE_ARTIFACTORY_PASSWORD}"
 
@@ -18,12 +18,10 @@ CURL_AUTH="-u ${NVTE_ARTIFACTORY_USER}:${NVTE_ARTIFACTORY_PASSWORD}"
 TE_ROOT="$(git rev-parse --show-toplevel)"
 ROCM_PATH="${ROCM_PATH:-/opt/rocm}"
 ROCM_VER="$(cut -d . -f1,2,3 < "${ROCM_PATH}/.info/version")"
-AITER_SHA="$(git -C "${TE_ROOT}/3rdparty/aiter" rev-parse --short=9 HEAD)"
-ARCHS="${NVTE_ROCM_ARCH:-gfx942;gfx950}"
-ARCHS_STR="$(echo "${ARCHS}" | sed 's/;/-/g')"
+AITER_SHA="$(git -C "${TE_ROOT}/3rdparty/aiter" rev-parse HEAD)"
 
 # Artifact key + remote path
-KEY="rocm-${ROCM_VER}_archs-${ARCHS_STR}_aiter-${AITER_SHA}"
+KEY="rocm-${ROCM_VER}_aiter-${AITER_SHA}"
 REMOTE_PATH="aiter-prebuilts/${KEY}.tar.gz"
 ARTIFACT_URL="${NVTE_PREBUILT_BASE_URL%/}/${REMOTE_PATH}"
 
@@ -36,23 +34,19 @@ TAR_PATH="${CACHE_ROOT}/${KEY}.tar.gz"
 [ -f "${TAR_PATH}" ] && rm -f -- "${TAR_PATH}"
 mkdir -p "${KEY_DIR}"
 
-# -------- upload: pack from local build and PUT to Artifactory ---------------
-  
-# Locate the built .so files in the build tree
+# --- Stage built artifacts ---
 FWD="$(echo "${TE_ROOT}"/build/lib.*-cpython-*/transformer_engine/lib/libmha_fwd.so | head -n 1)"
 BWD="$(echo "${TE_ROOT}"/build/lib.*-cpython-*/transformer_engine/lib/libmha_bwd.so | head -n 1)"
 
-[ -f "${FWD}" ] || { echo "[AITER-PREBUILT][ERROR] Missing libmha_fwd.so" >&2; exit 5; }
-[ -f "${BWD}" ] || { echo "[AITER-PREBUILT][ERROR] Missing libmha_bwd.so" >&2; exit 6; }
+[ -f "${FWD}" ] || { echo "[AITER-PREBUILT][ERROR] Missing libmha_fwd.so" >&2; exit 2; }
+[ -f "${BWD}" ] || { echo "[AITER-PREBUILT][ERROR] Missing libmha_bwd.so" >&2; exit 2; }
 
-# Stage files under KEY_DIR/
 cp -f "${FWD}" "${KEY_DIR}/"
 cp -f "${BWD}" "${KEY_DIR}/"
 
 # Zip and upload to artifactory
 tar -czf "${TAR_PATH}" -C "${CACHE_ROOT}" "${KEY}"
-echo "[AITER-PREBUILT] Started uploading ${REMOTE_PATH}"
+echo "[AITER-PREBUILT] Started uploading ${REMOTE_PATH} ..."
 echo "Artifactory response:"
 curl -fSL ${CURL_AUTH} -T "${TAR_PATH}" "${ARTIFACT_URL}" -w '\n'
 echo "[AITER-PREBUILT] Upload complete."
-exit 0
