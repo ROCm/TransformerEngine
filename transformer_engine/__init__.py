cf783d2e7f31465ec131508b2a6825a903520e0a
Meekail Zain <zainmeekail@gmail.com>
2025-10-28 13:24:07 -0500

    Expanded catch categories

--- transformer_engine/__init__.py
+++ transformer_engine/__init__.py
@@ -30,12 +30,12 @@ if os.getenv("NVTE_FRAMEWORK"):
 
 try:
     if _use_pytorch: from . import pytorch
-except (ImportError, FileNotFoundError):
+except (ImportError, FileNotFoundError, RuntimeError):
     pass
 
 try:
     if _use_jax: from . import jax
-except (ImportError, FileNotFoundError):
+except (ImportError, FileNotFoundError, RuntimeError):
     pass
 
 try:
