c293b6743c4b5a5447f794e2fb64ae49e3f7f04c
Matthias Diener <matthias.diener@gmail.com>
2025-10-29 13:15:05 -0500

    Enable test_float8_current_scaling_exact.py tests

--- ci/pytorch.sh
+++ ci/pytorch.sh
@@ -81,6 +81,7 @@ run_test_config(){
     run_default_fa 1 test_parallel_cross_entropy.py
     NVTE_USE_DEQUANTIZE_TRITON=1 NVTE_USE_CAST_TRANSPOSE_TRITON=1 NVTE_USE_RMSNORM_TRITON=1 NVTE_USE_LAYERNORM_TRITON=1 run_default_fa_lbl "triton" 1 test_numerics.py
     NVTE_USE_RMSNORM_TRITON=1 run_default_fa_lbl "triton" 1 test_fusible_ops.py
+    NVTE_USE_CAST_TRANSPOSE_TRITON=1 run_default_fa_lbl "triton" 1 test_float8_current_scaling_exact.py
 }
 
 run_test_config_mgpu(){
