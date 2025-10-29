8d66de5ccaf0f01656cd2fd15db65cdc1dd29765
Veera Gopu <VeeraRajasekharReddy.Gopu@amd.com>
2025-10-29 07:40:11 +0000

    Addressed comments, offloaded upload to users, updated cache check

--- transformer_engine/common/ck_fused_attn/aiter_prebuilt.cmake
+++ transformer_engine/common/ck_fused_attn/aiter_prebuilt.cmake
@@ -16,34 +16,26 @@ string(REGEX MATCH "^[0-9]+\\.[0-9]+" ROCM_VER "${ROCM_VER_CONTENT}")
 
 # AITER commit
 execute_process(
-  COMMAND git -C "${CMAKE_SOURCE_DIR}/../../3rdparty/aiter" rev-parse HEAD
+  COMMAND git -C "${CMAKE_CURRENT_LIST_DIR}/../../../3rdparty/aiter" rev-parse HEAD
   OUTPUT_VARIABLE AITER_SHA OUTPUT_STRIP_TRAILING_WHITESPACE
 )
 
 # Cache key & local paths
 set(KEY "rocm-${ROCM_VER}_aiter-${AITER_SHA}")
-set(CACHE_ROOT "${CMAKE_SOURCE_DIR}/../../build/aiter-prebuilts")
+set(CACHE_ROOT "${CMAKE_CURRENT_LIST_DIR}/../../../build/aiter-prebuilts")
 set(EXTRACT_DIR "${CACHE_ROOT}/${KEY}")
-
-# Initialize FILE_URL
-function(init_aiter_base_url )
-  
-endfunction()
 
 # Validate existing cache path
 function(is_aiter_cache_valid CACHE_VALID)
-  if(EXISTS "${__AITER_MHA_PATH}/libmha_fwd.so" AND EXISTS "${__AITER_MHA_PATH}/libmha_bwd.so")
-    set(EXPECTED_CACHE_PATH "${EXTRACT_DIR}")    
-    if(__AITER_MHA_PATH STREQUAL EXPECTED_CACHE_PATH)
-      set(${CACHE_VALID} TRUE PARENT_SCOPE)
-      message(STATUS "[AITER-PREBUILT] Using Cached __AITER_MHA_PATH=${__AITER_MHA_PATH}")
-      return()
-    endif()
+  if(EXISTS "${EXTRACT_DIR}/libmha_fwd.so" AND EXISTS "${EXTRACT_DIR}/libmha_bwd.so")
+    set(${CACHE_VALID} TRUE PARENT_SCOPE)
+    message(STATUS "[AITER-PREBUILT] Found Cached build files at ${EXTRACT_DIR}")
+    return()
   endif()
 
   # Cache is invalid/outdated - clean it
-  file(REMOVE_RECURSE "${EXTRACT_DIR}")
-  unset(__AITER_MHA_PATH CACHE)
+  file(REMOVE_RECURSE "${CACHE_ROOT}")
+  file(REMOVE_RECURSE "${CMAKE_CURRENT_LIST_DIR}/../../../build/cmake/_deps")
 endfunction()
 
 # Cache locally built libs
@@ -53,16 +45,13 @@ function(cache_local_aiter_build SOURCE_DIR)
   file(COPY "${SOURCE_DIR}/libmha_fwd.so" "${SOURCE_DIR}/libmha_bwd.so" DESTINATION "${EXTRACT_DIR}")
 endfunction()
 
-# Download prebuilt zip file
+# Download prebuilt tgz file
 function(download_aiter_prebuilt DOWNLOAD_SUCCESS)
   if(NOT DEFINED ENV{NVTE_AITER_PREBUILT_BASE_URL} OR "$ENV{NVTE_AITER_PREBUILT_BASE_URL}" STREQUAL "")
     return()
   endif()
 
-  set(BASE_URL "$ENV{NVTE_AITER_PREBUILT_BASE_URL}")
-  string(REGEX REPLACE "/$" "" BASE_URL "${BASE_URL}")
-  set(FILE_URL "${BASE_URL}/${KEY}.tar.gz")
-
+  string(REGEX REPLACE "/$" "" FILE_URL "$ENV{NVTE_AITER_PREBUILT_BASE_URL}/${KEY}.tar.gz")
   message(STATUS "[AITER-PREBUILT] NVTE_AITER_PREBUILT_BASE_URL is set - Attempting to download ${KEY}.tar.gz ...")
 
   # Check if ${KEY}.tar.gz exists in the URL provided.
@@ -86,81 +75,48 @@ function(download_aiter_prebuilt DOWNLOAD_SUCCESS)
 
   # Download & extract prebuilt files
   FetchContent_MakeAvailable(aiter_prebuilt)
-  
-  if(EXISTS "${EXTRACT_DIR}/libmha_fwd.so" AND EXISTS "${EXTRACT_DIR}/libmha_bwd.so")
-    message(STATUS "[AITER-PREBUILT] Successfully downloaded.")
-    set(${DOWNLOAD_SUCCESS} TRUE PARENT_SCOPE)
-  else()
-    file(REMOVE_RECURSE "${CACHE_ROOT}")
-    message(STATUS "[AITER-PREBUILT] Download unsuccessfull.")
-  endif()
+  message(STATUS "[AITER-PREBUILT] Successfully downloaded.")
+  set(${DOWNLOAD_SUCCESS} TRUE PARENT_SCOPE)
 endfunction()
 
-# Upload prebuilt
-# Requires: NVTE_AITER_PREBUILT_BASE_URL
-# Uses __AITER_MHA_PATH contents to create zip file and upload
-function(upload_aiter_prebuilt)  
-  if(NOT DEFINED ENV{NVTE_AITER_PREBUILT_BASE_URL} OR "$ENV{NVTE_AITER_PREBUILT_BASE_URL}" STREQUAL "")
-    message(FATAL_ERROR " [AITER-PREBUILT] ENV variable NVTE_AITER_PREBUILT_BASE_URL must be set.")
-  endif()
-
-  set(BASE_URL "$ENV{NVTE_AITER_PREBUILT_BASE_URL}")
-  string(REGEX REPLACE "/$" "" BASE_URL "${BASE_URL}")
-  set(FILE_URL "${BASE_URL}/${KEY}.tar.gz")
-
+# Create prebuilt tgz file to upload
+function(create_upload_files)
   # Locate .so files
-  if (NOT EXISTS  "${__AITER_MHA_PATH}/libmha_fwd.so")
+  if (NOT EXISTS  "${EXTRACT_DIR}/libmha_fwd.so")
     message(FATAL_ERROR "[AITER-PREBUILT] Missing libmha_fwd.so")
   endif()
-  if (NOT EXISTS  "${__AITER_MHA_PATH}/libmha_fwd.so")
+  if (NOT EXISTS  "${EXTRACT_DIR}/libmha_fwd.so")
     message(FATAL_ERROR "[AITER-PREBUILT] Missing libmha_bwd.so")
   endif()
 
+  # Output paths
+  set(OUTPUT_TGZ "/tmp/${KEY}.tar.gz")
+  set(OUTPUT_SHA "/tmp/${KEY}.tar.gz.sha256")
+
+  message(STATUS "[AITER-PREBUILT] Creating prebuilt files...")
   # Create archive
   file(ARCHIVE_CREATE
-       OUTPUT "/tmp/${KEY}.tar.gz"
+       OUTPUT "${OUTPUT_TGZ}"
        PATHS "${KEY}"
-       WORKING_DIRECTORY "/tmp"
+       WORKING_DIRECTORY "${CACHE_ROOT}"
        FORMAT "gnutar"
        COMPRESSION "GZip")
 
-  message(STATUS "[AITER-PREBUILT] Uploading /tmp/${KEY}.tar.gz ...")
-  set(TOKEN $ENV{NVTE_AITER_ACCESS_TOKEN})
-  if(NOT TOKEN STREQUAL "")
-    file(UPLOAD "/tmp/${KEY}.tar.gz" "${FILE_URL}"
-         HTTPHEADER "Authorization: Bearer ${TOKEN}"
-         SHOW_PROGRESS
-         STATUS upload_status
-         LOG upload_log
-         INACTIVITY_TIMEOUT 60
-         TIMEOUT 300)
-  else()
-    file(UPLOAD "/tmp/${KEY}.tar.gz" "${FILE_URL}"
-         SHOW_PROGRESS
-         STATUS upload_status
-         LOG upload_log
-         INACTIVITY_TIMEOUT 60
-         TIMEOUT 300)
-  endif()
-
-  list(GET upload_status 0 upload_code)
-  list(GET upload_status 1 upload_err)
-  if(upload_code EQUAL 0)
-    message(STATUS "[AITER-PREBUILT] Upload complete.")
-  else()
-    message(FATAL_ERROR "[AITER-PREBUILT] Upload failed.")
-  endif()
-
-  file(REMOVE "/tmp/${KEY}.tar.gz")
+  # Compute SHA256
+  file(SHA256 "${OUTPUT_TGZ}" ARCHIVE_HASH)
+  file(WRITE "${OUTPUT_SHA}" "${ARCHIVE_HASH}\n")
+  message(STATUS "[AITER-PREBUILT] tgz and sha256 files generated successfully:")
+  message(STATUS "  ${OUTPUT_TGZ}")
+  message(STATUS "  ${OUTPUT_SHA}")
 endfunction()
 
 # ------------------------------------------------------
-# Script-mode entry point (for manual upload)
-# Usage: cmake -P aiter_prebuilt.cmake -DACTION=upload
+# Script-mode entry point (to create upload files)
+# Usage: cmake -DACTION=upload -P /path/to/aiter_prebuilt.cmake
 # ------------------------------------------------------
 if (CMAKE_SCRIPT_MODE_FILE)
   if (DEFINED ACTION AND ACTION STREQUAL "upload")
-    upload_aiter_prebuilt()
+    create_upload_files()
   else()
     message(FATAL_ERROR "[AITER-PREBUILT] Invalid ACTION=${ACTION}. Use upload.")
   endif()
