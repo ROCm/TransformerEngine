d968f93eb4042c8aebb3acc746d3ac4a962b349d
Ye Wang <yewang12@amd.com>
2025-11-02 04:13:02 +0000

    [ROCm] resolve pytorch pytest conflicts

--- tests/pytorch/test_fusible_ops.py
+++ tests/pytorch/test_fusible_ops.py
@@ -28,15 +28,11 @@ from transformer_engine.pytorch.ops.fused import (
     ForwardLinearBiasAdd,
 )
 from transformer_engine.pytorch.tensor import QuantizedTensor
-<<<<<<< HEAD
-from transformer_engine.pytorch.tensor.float8_tensor import Float8Tensor, Float8Quantizer
-=======
 from transformer_engine.pytorch.tensor.float8_tensor import (
     Float8Tensor,
     Float8CurrentScalingQuantizer,
     Float8Quantizer,
 )
->>>>>>> ca7407e
 from transformer_engine.pytorch.tensor.mxfp8_tensor import MXFP8Tensor, MXFP8Quantizer
 from transformer_engine.pytorch.utils import is_bf16_compatible
 import transformer_engine_torch as tex
