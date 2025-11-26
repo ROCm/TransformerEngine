0fdff864e2f7304ff510d9c935ef97770d3307f8
Meekail Zain <zainmeekail@gmail.com>
2025-11-26 12:52:28 -0600

    Moved handling of env variable to makefile

--- setup.py
+++ setup.py
@@ -73,8 +73,6 @@ def setup_common_extension() -> CMakeExtension:
             cmake_flags.append("-DUSE_FUSED_ATTN_AOTRITON=OFF")
         else:
             os.environ["AOTRITON_CI_SUPPLIED_SHA1"] = "98371989e8a23267e284c94e95156a139e4b33c4"
-            if int(os.getenv("NVTE_AOTRITON_BUILD_GPU_KERNELS", "0")):
-                cmake_flags.append("-DUSE_FUSED_ATTN_AOTRITON_BUILD_GPU_KERNELS=ON")
         if int(os.getenv("NVTE_FUSED_ATTN_CK", "1"))==0 or int(os.getenv("NVTE_FUSED_ATTN", "1"))==0:
             cmake_flags.append("-DUSE_FUSED_ATTN_CK=OFF")
         if bool(int(os.getenv("NVTE_ENABLE_NVSHMEM", "0"))) and os.getenv("NVTE_ENABLE_ROCSHMEM") is None:
