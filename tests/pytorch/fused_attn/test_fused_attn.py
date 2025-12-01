0018b1bce0240ad53a6ac24a3b8052d9f067d34b
Meekail Zain <34613774+Micky774@users.noreply.github.com>
2025-12-01 14:23:51 -0500

    PyTorch FA test fix (#370)
    * Corrected bugs
    
    * Updated documentation

--- tests/pytorch/fused_attn/test_fused_attn.py
+++ tests/pytorch/fused_attn/test_fused_attn.py
@@ -220,8 +220,8 @@ def _get_attention_backends(
         )
         (
             use_flash_attention,
-            use_fused_attention,
             flash_attention_backend,
+            use_fused_attention,
             fused_attention_backend,
             use_unfused_attention,
             available_backends,
@@ -369,6 +369,7 @@ def test_dot_product_attention(
             and config.attn_mask_type in ["causal", "padding_causal"]
         )
         and (config.window_size[0] == -1 or FlashAttentionUtils.v2_3_plus)
+        and not is_mla
     ):
         flash_attn_supported = True
 
