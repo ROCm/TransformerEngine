[ROCm] apply more strict filtering for just cross-attn and fix the softmax shape bug

transformer_engine/jax/cpp_extensions/attention.py
@@ -359,21 +359,21 @@ class FusedAttnFwdPrimitive(BasePrimitive):
             elif backend == NVTE_Fused_Attn_Backend.NVTE_CK:
                 if config.qkv_layout.is_thd():
                     # THD only: check env; run small-seq logic only when enabled
-                    if os.environ.get("NVTE_FUSED_ATTN_CK_SMALLSEQ", "0") != "1":
+                    if os.environ.get("NVTE_FUSED_ATTN_CK_SMALLSEQ", "0") != "1" or q_max_seqlen == kv_max_seqlen:
                         softmax_shape = (*batch_shape, q_max_seqlen, attn_heads, 1)
                         softmax_dtype = dtypes.canonicalize_dtype(jnp.float32)
                     else:
                         batch_size = reduce(operator.mul, batch_shape)
                         ck_standard_softmax_aux_size = (
-                            batch_size * attn_heads * q_max_seqlen * 1
-                        )
+                            batch_size * attn_heads * q_max_seqlen * 1 * 4 
+                        ) # 4 bytes for the float32
                         ck_smallseq_softmax_aux_size = (
                             batch_size * attn_heads * q_max_seqlen
                             * min(kv_max_seqlen, 16) * 2
                         )  # 2 bytes for bf16/fp16
                         if ck_standard_softmax_aux_size >= ck_smallseq_softmax_aux_size:
                             softmax_shape = (*batch_shape, attn_heads, q_max_seqlen, 1)
-                            softmax_dtype = dtypes.canonicalize_dtype(q_dtype)
+                            softmax_dtype = dtypes.canonicalize_dtype(jnp.float32)
                         else:
                             softmax_shape = (*batch_shape, attn_heads, q_max_seqlen, min(kv_max_seqlen, 16))
                             softmax_dtype = dtypes.canonicalize_dtype(q_dtype)