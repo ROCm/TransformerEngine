e2fe39ec6fe435bc32a68699ef9982a714b06026
sudhu2k <Sudharshan.Govindan@amd.com>
2026-04-24 02:21:30 +0000

    Revert removal of fp8_format from MXFP4 recipe

--- transformer_engine/common/recipe/__init__.py
+++ transformer_engine/common/recipe/__init__.py
@@ -574,6 +574,7 @@ class MXFP4BlockScaling(Recipe):
 
     margin: int = 0
     fp4_format: Format = Format.E2M1
+    fp8_format: Format = Format.E4M3
     fp8_dpa: bool = False
     fp8_mha: bool = False
     use_hadamard: bool = os.getenv("NVTE_MXFP4_USE_HADAMARD", "0") == "1"
