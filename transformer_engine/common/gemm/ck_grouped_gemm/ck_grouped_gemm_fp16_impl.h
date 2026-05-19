01f62d08e3575009d2e4581e9a4e0d58e2deda55
Matthias Diener <matthias.diener@gmail.com>
2026-05-19 16:22:37 +0000

    factor out templating

--- transformer_engine/common/gemm/ck_grouped_gemm/ck_grouped_gemm_fp16_impl.h
+++ transformer_engine/common/gemm/ck_grouped_gemm/ck_grouped_gemm_fp16_impl.h
@@ -231,6 +231,42 @@ class GroupedGemmRunner : public RunnerInterface {
     runner = std::make_unique<Runner>();                               \
   })
 
+// Templated dispatch on A/B layouts, shared by all layout-specific .cpp files.
+template <typename ALayout, typename BLayout>
+bool ck_tile_grouped_gemm_fp16_dispatch_layout(DType a_dtype, DType d_dtype,
+                                               bool need_m_pad, bool need_k_pad,
+                                               const GroupedGemmRunContext& ctx) {
+  const ck_tile::stream_config s{ctx.stream};
+  std::unique_ptr<RunnerInterface> runner = nullptr;
+
+  TRANSFORMER_ENGINE_TYPE_SWITCH_16BIT(a_dtype, a_te_type, {
+    using AType = typename TETypeToCKType<a_te_type>::type;
+    using BType = typename TETypeToCKType<a_te_type>::type;
+    using CLayout = RowMajor;
+
+    TRANSFORMER_ENGINE_TYPE_SWITCH_NON_FP8ONLY(d_dtype, d_te_type, {
+      using CType = typename TETypeToCKType<d_te_type>::type;
+
+      TRANSFORMER_ENGINE_SWITCH_CONDITION(need_m_pad, kPadM, {
+        TRANSFORMER_ENGINE_SWITCH_CONDITION(need_k_pad, kPadK, {
+          if (ctx.N % 256 == 0) {
+            MAKE_RUNNER(TileCfg_256x256x64, kPadM, false, kPadK);
+          } else if (ctx.N % 128 == 0) {
+            MAKE_RUNNER(TileCfg_256x128x64, kPadM, false, kPadK);
+          } else {
+            MAKE_RUNNER(TileCfg_256x128x64, kPadM, true, kPadK);
+          }
+        });
+      });
+    });
+  });
+
+  if (!runner) {
+    return false;
+  }
+  return runner->run(s, ctx);
+}
+
 // Per-layout dispatch function signature.
 // Each layout file (NN, NT, TN, TT) implements one of these.
 bool ck_tile_grouped_gemm_fp16_dispatch_nn(DType a_dtype, DType d_dtype,
--- transformer_engine/common/gemm/ck_grouped_gemm/ck_grouped_gemm_fp16_tt.cpp
+++ transformer_engine/common/gemm/ck_grouped_gemm/ck_grouped_gemm_fp16_tt.cpp
@@ -12,41 +12,9 @@ namespace grouped_gemm {
 bool ck_tile_grouped_gemm_fp16_dispatch_tt(DType a_dtype, DType d_dtype,
                                            bool need_m_pad, bool need_k_pad,
                                            const GroupedGemmRunContext& ctx) {
-  const ck_tile::stream_config s{ctx.stream};
-  std::unique_ptr<RunnerInterface> runner = nullptr;
-
-  using ALayout = ColMajor;
-  using BLayout = ColMajor;
-
-  TRANSFORMER_ENGINE_TYPE_SWITCH_16BIT(a_dtype, a_te_type, {
-    using AType = typename TETypeToCKType<a_te_type>::type;
-    using BType = typename TETypeToCKType<a_te_type>::type;
-    using CLayout = RowMajor;
-
-    TRANSFORMER_ENGINE_TYPE_SWITCH_NON_FP8ONLY(d_dtype, d_te_type, {
-      using CType = typename TETypeToCKType<d_te_type>::type;
-
-      TRANSFORMER_ENGINE_SWITCH_CONDITION(need_m_pad, kPadM, {
-        TRANSFORMER_ENGINE_SWITCH_CONDITION(need_k_pad, kPadK, {
-          if (ctx.N % 256 == 0) {
-            MAKE_RUNNER(TileCfg_256x256x64, kPadM, false, kPadK);
-          } else if (ctx.N % 128 == 0) {
-            MAKE_RUNNER(TileCfg_256x128x64, kPadM, false, kPadK);
-          } else {
-            MAKE_RUNNER(TileCfg_256x128x64, kPadM, true, kPadK);
-          }
-        });
-      });
-    });
-  });
-
-  if (!runner) {
-    return false;
-  }
-  return runner->run(s, ctx);
+  return ck_tile_grouped_gemm_fp16_dispatch_layout<ColMajor, ColMajor>(
+      a_dtype, d_dtype, need_m_pad, need_k_pad, ctx);
 }
-
-#undef MAKE_RUNNER
 
 }  // namespace grouped_gemm
 }  // namespace transformer_engine
