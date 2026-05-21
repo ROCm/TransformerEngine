e3fe90b7a1bd106dbf44b3eecec18c112806068a
Aristotle <89488299+aris134@users.noreply.github.com>
2026-05-21 19:59:36 -0400

    CK Tile MXFP8 Group GEMM gfx1250 (#578)

--- transformer_engine/common/gemm/ck_grouped_gemm/ck_grouped_gemm_common.h
+++ transformer_engine/common/gemm/ck_grouped_gemm/ck_grouped_gemm_common.h
@@ -9,6 +9,7 @@
 #include <hip/hip_runtime.h>
 #include "common/util/cuda_runtime.h"
 
+#include <algorithm>
 #include <array>
 #include <type_traits>
 #include <vector>
@@ -20,6 +21,9 @@
 #include "ck_tile/core.hpp"
 #include "ck_tile/ops/epilogue.hpp"
 #include "ck_tile/ops/gemm.hpp"
+#include "ck_tile/host/kernel_launch.hpp"
+#include "ck_tile/ops/gemm/kernel/mx_grouped_gemm_kernel.hpp"
+#include "ck_tile/ops/elementwise/unary_element_wise_operation.hpp"
 
 namespace transformer_engine {
 namespace grouped_gemm {
