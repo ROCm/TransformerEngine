4b017fe96f27e38cb6403f236174753ed3ac0d9a
Aristotle Martin <Aristotle.Martin@amd.com>
2026-06-11 19:08:11 +0000

    pass in 0 to sm_arch in detect_gpu_arch

--- transformer_engine/common/gemm/ck_grouped_gemm/ck_grouped_gemm_common.h
+++ transformer_engine/common/gemm/ck_grouped_gemm/ck_grouped_gemm_common.h
@@ -85,7 +85,7 @@ enum class GPUArch {
 };
 
 static inline GPUArch detect_gpu_arch() {
-  switch (cuda::sm_arch()) {
+  switch (cuda::sm_arch(0)) {
     case 94:
       return GPUArch::GFX942;
     case 95:
