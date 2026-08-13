f5080819def9ed996f913cad5f5e86d53955dcd4
Gene Der Su <e870252314@gmail.com>
2026-08-13 10:04:41 -0700

    add back rocm7 as default and enable extra rocm10 installations
    Signed-off-by: Gene Der Su <e870252314@gmail.com>
    (cherry picked from commit 4f39b1c30b08add9808b0ad9d54dd9458ceb09ef)

--- setup.py
+++ setup.py
@@ -430,10 +430,11 @@ if __name__ == "__main__":
             "pytorch": [f"transformer_engine_torch=={__version__}"],
             "jax": [f"transformer_engine_jax=={__version__}"],
         } if not rocm_build() else {
-            "rocm": [f"transformer_engine_rocm10=={__version__}"],
+            "rocm": [f"transformer_engine_rocm7=={__version__}"],
+            "rocm7": [f"transformer_engine_rocm7=={__version__}"],
             "rocm10": [f"transformer_engine_rocm10=={__version__}"],
-            "rocm_pytorch": [f"transformer_engine_rocm10[pytorch]=={__version__}"],
-            "rocm_jax": [f"transformer_engine_rocm10[jax]=={__version__}"],
+            "rocm_pytorch": [f"transformer_engine_rocm_torch=={__version__}"],
+            "rocm_jax": [f"transformer_engine_rocm_jax=={__version__}"],
         }
     else:
         install_requires, test_requires = setup_requirements()
