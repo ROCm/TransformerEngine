6dd8135be2dd25578fcc4057a27223265de011d2
ipanfilo <145064111+ipanfilo@users.noreply.github.com>
2026-09-16 19:32:17 -0400

    GFX1250 changes with updated AITER (#732)
    * Update to new QoLA/CK-JIT/AITER
    * Update CI prebuild CK blob list
    * Skip known failing test config
    * Gfx1250 changes on top of updated AITER
    * Enable CK tests. Enable MXFP4, MXFP8 and NVFP4 in core, still disabled for FW integration
    * Split-kv and sink kernels usage honor V3 enablement flag
    * Add synchronize to mem_calc test.
    * gfx1250-strict related changes: clean/replace __gfx1250__ with __GFX12__

--- transformer_engine/common/fused_attn_rocm/fused_attn_ck.cpp
+++ transformer_engine/common/fused_attn_rocm/fused_attn_ck.cpp
@@ -290,7 +290,7 @@ __forceinline__ __device__ int binary_search(int32_t target, const int32_t *arra
   return left - 1;
 }
 
-#if !defined(__HIP_DEVICE_COMPILE__) || defined(__gfx1250__)
+#if !defined(__HIP_DEVICE_COMPILE__) || defined(__GFX12__)
 constexpr int THREADS_PER_WAVEFRONT = 32;
 #else
 constexpr int THREADS_PER_WAVEFRONT = 64;
@@ -564,7 +564,7 @@ void fused_attn_ck_fwd_impl(
     ck_args.cu_seqlen_q_ptr = devPtrCuSeqlensQ;
   }
 
-  ck_args.num_splits = ck_attn_fwd_num_splits(ck_args);
+  ck_args.num_splits = ck_args.uses_fwd_v3 ? ck_attn_fwd_num_splits(ck_args) : 0;
   if (ck_args.num_splits > 0)
   {
     size_t splitkv_workspace_bytes = ck_attn_fwd_workspace_size(ck_args);
